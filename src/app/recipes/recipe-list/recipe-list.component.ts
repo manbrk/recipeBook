b818b4838792a826d2abcda6b449e9312511e8b2
manbrk <manbrk@gmail.com>
2017-02-17 23:49:41 +0200

    Добавлен сервис recipe.service из которого берутся рецепты, вместо recipe-list.component.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,21 @@
 import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../recipe";
+import {RecipeService} from "../recipe.service";
 
 @Component({
     selector: 'rb-recipe-list',
     templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-    recipes: Recipe[] = [
-        new Recipe('Schnitzel', 'Very tasty', 'http://www.bbcgoodfood.com/sites/default/files/styles/carousel_small/public/recipe/recipe-image/2016/03/chicken-schnitzel-strips-with-tomato-spaghetti.jpg?itok=3r23piTE', []),
-        new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
-    ];
+    recipes: Recipe[] = [];
+
     @Output() recipeSelected = new EventEmitter<Recipe>();
 
-    constructor() {
+    constructor(private recipeService: RecipeService) {
     }
 
     ngOnInit() {
+        this.recipes = this.recipeService.getRecipes();
     }
 
     onSelected(recipe: Recipe) {
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Recipe} from "./recipe";
+
+@Injectable()
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('Schnitzel', 'Very tasty', 'http://www.bbcgoodfood.com/sites/default/files/styles/carousel_small/public/recipe/recipe-image/2016/03/chicken-schnitzel-strips-with-tomato-spaghetti.jpg?itok=3r23piTE', []),
+        new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+    ];
+
+  constructor() { }
+
+  getRecipes() {
+      return this.recipes;
+  }
+
+}
