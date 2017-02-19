e4a4cd4b5f10ebc1ec7b4d5cfd6ecdf79718992b
manbrk <manbrk@gmail.com>
2017-02-19 16:23:30 +0200

    Навигация и роутинг работают. Также работает кнопка Delete.

--- src/app/recipes/recipe-list/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 import {Recipe} from '../recipe';
 
@@ -6,14 +6,8 @@ import {Recipe} from '../recipe';
     selector: 'rb-recipe-item',
     templateUrl: './recipe-item.component.html'
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent {
     @Input() recipe: Recipe;
-    recipeId: number = 1;
-
-    constructor() {
-    }
-
-    ngOnInit() {
-    }
+    @Input() recipeId: number = 1;
 
 }
