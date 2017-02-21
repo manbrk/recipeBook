8dd4a2c2ba76d0138703a2849368bd63c1fac3e6
manbrk <manbrk@gmail.com>
2017-02-21 18:08:26 +0200

    Кнопка ToShoppingList добавляет ингридиенты в список, который показывает количество значений в форме.

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {Ingridient} from "../shared/ingridient";
 import {ShoppingListService} from "./shopping-list.service";
 
@@ -6,14 +6,20 @@ import {ShoppingListService} from "./shopping-list.service";
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
-  item: Ingridient;
+export class ShoppingListAddComponent implements OnChanges {
+  @Input() item: Ingridient;
   isAdd = true;
 
   constructor(private sls: ShoppingListService) { }
 
-  ngOnInit() {
-  }
+    ngOnChanges(changes) {
+      if (changes.item.currentValue === null) {
+          this.isAdd = true;
+          this.item = {name: null, amount: null};
+      } else {
+          this.isAdd = false;
+      }
+    }
 
   onSubmit(ingridient: Ingridient) {
     if (!this.isAdd) {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,11 +8,16 @@ import {ShoppingListService} from "./shopping-list.service";
 })
 export class ShoppingListComponent implements OnInit {
     items: Ingridient[] = [];
+    selectedItem: Ingridient = null;
 
   constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
     this.items = this.sls.getItems();
+  }
+
+  onSelectItem(item: Ingridient) {
+    this.selectedItem = item;
   }
 
 }
